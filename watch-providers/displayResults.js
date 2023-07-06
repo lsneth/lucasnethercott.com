96f3b240fe129b85c333ae4da1dfbcdc902157c2
Lucas <luke@nethercott.com>
2023-07-06 17:43:18 -0600

    fix tv show display bug

--- watch-providers/displayResults.js
+++ watch-providers/displayResults.js
@@ -29,7 +29,7 @@ export default async function displayShows(showType, searchQuery) {
 function showTemplate(show, showType) {
   const image = `https://image.tmdb.org/t/p/w300${show.poster_path}`
   return `
-      <a href='show.html?showId=${show.id}&showType=${showType}&title=${show.name ?? show.original_title}&releaseYear=${show.release_date.slice(0, 4)}&imageUrl=${image}' class='show'>
+      <a href='show.html?showId=${show.id}&showType=${showType}&title=${showType === 'tv' ? show.name : show.original_title}&releaseYear=${showType === 'tv' ? show.first_air_date.slice(0, 4) : show.release_date.slice(0, 4)}&imageUrl=${image}' class='show'>
         <img src='${image}'/>
       </a>
     `
@@ -38,8 +38,7 @@ function showTemplate(show, showType) {
 // removes search results
 function removeResults() {
   if (document.querySelector('.show') || document.querySelector('#noResultsString')) {
-    const shows = document.querySelectorAll('.show')
-    shows.forEach((show) => show.remove())
-    document.querySelector('#noResultsString').remove()
+    document.querySelectorAll('.show')?.forEach((show) => show.remove())
+    document.querySelector('#noResultsString')?.remove()
   }
 }
