0ce2e84b349c3c0645788fcf5b48adfcb6941e7f
Lucas <luke@nethercott.com>
2024-04-15 20:59:49 -0600

    temp logo and favicon

--- src/app/_components/NavBar.tsx
+++ src/app/_components/NavBar.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import NavItem from './NavItem'
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function NavBar() {
   const navItemProps = [
@@ -11,8 +12,16 @@ export default function NavBar() {
   ]
 
   return (
-    <nav className="flex justify-end m-5 text-right">
-      <Image src="/logo.jpg" alt="lucasnethercott.com logo" width={32} height={32} className="flex-1 max-w-8 max-h-8" />
+    <nav className="flex justify-end items-center my-5 mx-8 text-right">
+      <Link href="/">
+        <Image
+          src="/logo.png"
+          alt="lucasnethercott.com logo"
+          width={32}
+          height={32}
+          className="flex-1 max-w-8 max-h-8"
+        />
+      </Link>
       <div className="flex-1">
         {navItemProps.map((props) => (
           <NavItem key={props.href} {...props} />
