3ae11a66488affad5a7e9b4f2baf963c3bd5b630
Lucas <luke@nethercott.com>
2024-04-18 06:42:07 -0600

    try new method

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,21 +21,21 @@ export default function Home() {
 
       <ProjectSection
         title="Mafia: Social Deduction (In Progress)"
-        pathToImage="/mafia.png"
+        pathToImage="./mafia.png"
         description="The social deduction genre at it's finest. No moderator. No cards. No noises at night. Just you, your friends, and pure social deduction. This mobile app will be a building block for a series of social deduction games to come."
         imageAltText="mafia: social deduction cover image"
         codeLink="https://github.com/lsneth/mafia-social-deduction"
       />
       <ProjectSection
         title="Sold Out"
-        pathToImage="/sold-out.png"
+        pathToImage="./sold-out.png"
         description="A tshirt company that only sells each tshirt once. Once it's sold, it's sold out. This is made possible through many modern technologies including recent developments in AI art generation."
         imageAltText="mafia: social deduction cover image"
         deployedLink="https://soldout.limited/"
       />
       <ProjectSection
         title="Stream Scout"
-        pathToImage="/stream-scout-logo.png"
+        pathToImage="./stream-scout-logo.png"
         description="A solution for quickly finding where a movie or TV show is currently available to stream, rent, or buy. Stream Scout leverages TMDB's API to provide a simple cinema search experience."
         imageAltText="stream scout logo"
         deployedLink="https://streamscout.lucasnethercott.com"
